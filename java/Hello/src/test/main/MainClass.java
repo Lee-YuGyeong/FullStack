b937c404273210176dfd5778e81b2b84a3ad258e
Lee-YuGyeong <60072190+Lee-YuGyeong@users.noreply.github.com>
2021-10-27 21:18:54 +0900

    step1

---
+++ java/Hello/src/test/main/MainClass.java
@@ -0,0 +1,12 @@
+package test.main;
+
+//MainClass 는 main 이라는 이름의 method를 하나 가지고 있다.
+public class MainClass {
+	/*
+	 * run (실행) 했을때 실행의 흐름이 시작되는 아주 특별한 모양의 main 메소드
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, World");
+		System.out.println("이유경");
+	}
+}
---
+++ java/Hello/src/test/main/MainClass2.java
@@ -0,0 +1,18 @@
+package test.main;
+
+public class MainClass2 {
+	
+	public static void main(String[] args) {
+		System.out.println("시작!");
+		play();
+		play2();
+		System.out.println("끝");
+	}
+	static void play() {
+		System.out.println("놀자~");
+		System.out.println("놀자~");
+	}
+	static void play2() {
+		System.out.println("놀자2~");
+	}
+}
