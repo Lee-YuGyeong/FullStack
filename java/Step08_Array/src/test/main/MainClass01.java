3d39ecd9170fe5e0e52027e4cfa1ec9cf9a8d433
Lee-YuGyeong <60072190+Lee-YuGyeong@users.noreply.github.com>
2021-11-01 22:11:43 +0900

    step8

---
+++ java/Step08_Array/src/test/main/MainClass01.java
@@ -0,0 +1,19 @@
+package test.main;
+
+import test.mypac.Speaker;
+
+public class MainClass01 {
+	public static void main(String[] args) {
+		//10,20,30,40,50를 배열겍체에 담고 그 객체의 참조값을 nums 라는 지역변수에 담기
+		 int[] nums = {10,20,30,40,50};
+		 double[] nums2 = {10.1,10.2,10.3,10.4,10.5};
+		 
+		 boolean[] truth = {true,false,false,true,true};
+		 
+		 String[] names = {"김구라","해골","원숭이","주뎅이","덩어리"};
+		 
+		 Speaker[] speakers = {new Speaker(),new Speaker(),new Speaker()};
+		 
+		 
+	}
+}
