3a3ff857080d483b936c968f87c1a1c7dade16b6
Lee-YuGyeong <60072190+Lee-YuGyeong@users.noreply.github.com>
2021-12-24 02:15:30 +0900

    spring boot

---
+++ spring/sprweb20boot/src/main/java/aa/bb/cc/Sprweb20bootApplication.java
@@ -0,0 +1,13 @@
+package aa.bb.cc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprweb20bootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprweb20bootApplication.class, args);
+	}
+
+}
---
+++ spring/sprweb20boot/src/test/java/aa/bb/cc/Sprweb20bootApplicationTests.java
@@ -0,0 +1,13 @@
+package aa.bb.cc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sprweb20bootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
